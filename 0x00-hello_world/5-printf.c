c86b8d93207371961e4f485ec2a9e3a337e10d73
raphaelnnadi <raphaelnnadi1994@gmail.com>
2022-09-08 16:17:31 -0700

    p

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Description: main - Prints out a string to stdout.
+ * Description: main - Prints out a string to stdout
  * 
- * Return: 0 if success.
+ * Return: 0 if success
  * 
  */
 int main(void)
