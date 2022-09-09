dcf6f34eca36ef87281828f99dbb2d4e16855d0c
raphaelnnadi <raphaelnnadi1994@gmail.com>
2022-09-08 18:03:10 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 /**
  * Description: main - Prints out a string to stdout
- * 
+ *
  * Return: 0 if success
- * 
+ *
  */
 int main(void)
 
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
